fc5c04c349bea728e6873f5b031dfbc8d61a619e
Wei Wang <wvw@google.com>
2025-01-15 21:58:47 -0800

    am: set to top-app process group only for instrument
    Bug: 379892006 
    Bug: 383813628
    Change-Id: I6cf725f659cb1936c40665e72e5f739dea66de0e
    Test: Build
    Flag: NONE for a script change

--- cmds/am/am.sh
+++ cmds/am/am.sh
@@ -1,11 +1,10 @@
 #!/system/bin/sh
 
-# set to top-app process group
-settaskprofile $$ SCHED_SP_TOP_APP >/dev/null 2>&1 || true
-
 if [ "$1" != "instrument" ] ; then
     cmd activity "$@"
 else
+    # set to top-app process group for instrument
+    settaskprofile $$ SCHED_SP_TOP_APP >/dev/null 2>&1 || true
     base=/system
     export CLASSPATH=$base/framework/am.jar
     exec app_process $base/bin com.android.commands.am.Am "$@"
