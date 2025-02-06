4124fe185535f6bf86f21838bde29672b3a81bf6
Jack Yu <jackyu@google.com>
2025-02-06 15:25:37 -0800

    Cleaned up the flag apn_setting_field_support_flag
    Cleaned up the 24Q3 flag apn_setting_field_support_flag
    
    Bug: 307038091
    Test: Basic telephony functionality tests
    Test: atest ApnSettingTest
    Flag: EXEMPT flag cleanup
    Change-Id: I784b52afd4682748f0b3a06bd6487c556c09955c

--- core/java/android/provider/Telephony.java
+++ core/java/android/provider/Telephony.java
@@ -17,7 +17,6 @@
 package android.provider;
 
 import android.Manifest;
-import android.annotation.FlaggedApi;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.RequiresPermission;
@@ -50,7 +49,6 @@ import android.text.TextUtils;
 import android.util.Patterns;
 
 import com.android.internal.telephony.SmsApplication;
-import com.android.internal.telephony.flags.Flags;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -3196,7 +3194,6 @@ public final class Telephony {
          * See 3GPP TS 23.501 section 5.6.13
          * <P>Type: INTEGER</P>
          */
-        @FlaggedApi(Flags.FLAG_APN_SETTING_FIELD_SUPPORT_FLAG)
         public static final String ALWAYS_ON = "always_on";
 
         /**
@@ -3307,7 +3304,6 @@ public final class Telephony {
          * connected, in bytes.
          * <p>Type: INTEGER </p>
          */
-        @FlaggedApi(Flags.FLAG_APN_SETTING_FIELD_SUPPORT_FLAG)
         public static final String MTU_V4 = "mtu_v4";
 
         /**
@@ -3315,7 +3311,6 @@ public final class Telephony {
          * connected, in bytes.
          * <p>Type: INTEGER </p>
          */
-        @FlaggedApi(Flags.FLAG_APN_SETTING_FIELD_SUPPORT_FLAG)
         public static final String MTU_V6 = "mtu_v6";
 
         /**
@@ -3338,14 +3333,12 @@ public final class Telephony {
          * {@code true} if this APN visible to the user, {@code false} otherwise.
          * <p>Type: INTEGER (boolean)</p>
          */
-        @FlaggedApi(Flags.FLAG_APN_SETTING_FIELD_SUPPORT_FLAG)
         public static final String USER_VISIBLE = "user_visible";
 
         /**
          * {@code true} if the user allowed to edit this APN, {@code false} otherwise.
          * <p>Type: INTEGER (boolean)</p>
          */
-        @FlaggedApi(Flags.FLAG_APN_SETTING_FIELD_SUPPORT_FLAG)
         public static final String USER_EDITABLE = "user_editable";
 
         /**
