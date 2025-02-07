96b38375be8697f511519d4351538553079c5af6
Dan Sandler <dsandler@android.com>
2025-02-07 00:26:15 -0500

    Seen while cruising the solar system:
        +---------------+ .
        | SPACE IS BIG, |    .
        +---------------+
         .     ||   +---------------+
      .        ||   | SPACE IS FAR. |
              ^^^^  +---------------+
         +-----------+  .  ||      . .
         | HOW ABOUT |     ||  .
         +-----------+    ^^^^
               ||  +-----------------+
         .     ||  | A PROGRESS BAR? |
              ^^^^ +-----------------+
       .            .      ||   .
           .     +------------+     .
                 | ANDROID 16 |
                 +------------+
           .           ||         .
                       ||   .
       .              ^^^^
    
    Bug: 373855388
    Test: adb shell am start -n com.android.egg/.landroid.MainActivity
    Flag: com.android.egg.flags.flag_flag
    Change-Id: I2d1e2b34d036f8b765d407087130a00cc7b7082e

--- packages/EasterEgg/src/com/android/egg/landroid/DreamUniverse.kt
+++ packages/EasterEgg/src/com/android/egg/landroid/DreamUniverse.kt
@@ -56,6 +56,8 @@ class DreamUniverse : DreamService() {
             }
         }
 
+    private var notifier: UniverseProgressNotifier? = null
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
@@ -76,8 +78,8 @@ class DreamUniverse : DreamService() {
                         Random.nextFloat() * PI2f,
                         Random.nextFloatInRange(
                             PLANET_ORBIT_RANGE.start,
-                            PLANET_ORBIT_RANGE.endInclusive
-                        )
+                            PLANET_ORBIT_RANGE.endInclusive,
+                        ),
                     )
         }
 
@@ -94,8 +96,10 @@ class DreamUniverse : DreamService() {
         composeView.setContent {
             Spaaaace(modifier = Modifier.fillMaxSize(), u = universe, foldState = foldState)
             DebugText(DEBUG_TEXT)
-            Telemetry(universe)
+            Telemetry(universe, showControls = false)
         }
+
+        notifier = UniverseProgressNotifier(this, universe)
 
         composeView.setViewTreeLifecycleOwner(lifecycleOwner)
         composeView.setViewTreeSavedStateRegistryOwner(lifecycleOwner)
