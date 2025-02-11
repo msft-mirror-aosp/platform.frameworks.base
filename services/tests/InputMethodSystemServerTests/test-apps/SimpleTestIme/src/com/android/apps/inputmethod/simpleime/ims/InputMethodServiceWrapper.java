ed465be2fafd9aee27477d50495c1e9fc365a01d
Cosmin Băieș <cosminbaies@google.com>
2025-02-11 12:35:29 +0100

    Include inputViewShown in verifyInputViewStatus
    Currently verifyInputViewStatus includes a few checks, but does not
    incldue checking isInputViewShown. This refactors the method to also
    check this value, to reduce duplication.
    
    Additionally re-orders the methods in TestActivity and
    InputMethodServiceWrapper, as well as storing the static instance of the
    wrapper in a weak reference.
    
    Flag: EXEMPT testfix
    Bug: 394328311
    Test: atest InputMethodServiceTest
    Change-Id: Idd844caadedec49b0165d3cf4ed6ca3e61638783

--- services/tests/InputMethodSystemServerTests/test-apps/SimpleTestIme/src/com/android/apps/inputmethod/simpleime/ims/InputMethodServiceWrapper.java
+++ services/tests/InputMethodSystemServerTests/test-apps/SimpleTestIme/src/com/android/apps/inputmethod/simpleime/ims/InputMethodServiceWrapper.java
@@ -27,6 +27,7 @@ import androidx.annotation.Nullable;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.ref.WeakReference;
 import java.util.concurrent.CountDownLatch;
 
 /** Wrapper of {@link InputMethodService} to expose interfaces for testing purpose. */
@@ -35,8 +36,8 @@ public class InputMethodServiceWrapper extends InputMethodService {
     private static final String TAG = "InputMethodServiceWrapper";
 
     /** Last created instance of this wrapper. */
-    @Nullable
-    private static InputMethodServiceWrapper sInstance;
+    @NonNull
+    private static WeakReference<InputMethodServiceWrapper> sInstance = new WeakReference<>(null);
 
     /** IME show event ({@link #onStartInputView}). */
     public static final int EVENT_SHOW = 0;
@@ -68,38 +69,23 @@ public class InputMethodServiceWrapper extends InputMethodService {
     @Nullable
     private CountDownLatch mCountDownLatch;
 
-    /** Gets the last created instance of this wrapper, if available. */
-    @Nullable
-    public static InputMethodServiceWrapper getInstance() {
-        return sInstance;
-    }
-
-    public boolean getCurrentInputViewStarted() {
-        return mInputViewStarted;
-    }
-
-    /**
-     * Sets the latch used to wait for the IME event.
-     *
-     * @param latch      the latch to wait on.
-     * @param latchEvent the event to set the latch on.
-     */
-    public void setCountDownLatchForTesting(@Nullable CountDownLatch latch, @Event int latchEvent) {
-        mCountDownLatch = latch;
-        mLatchEvent = latchEvent;
-    }
-
     @Override
     public void onCreate() {
         Log.i(TAG, "onCreate()");
         super.onCreate();
-        sInstance = this;
+        sInstance = new WeakReference<>(this);
     }
 
     @Override
     public void onStartInput(EditorInfo info, boolean restarting) {
         Log.i(TAG, "onStartInput() editor=" + dumpEditorInfo(info) + ", restarting=" + restarting);
         super.onStartInput(info, restarting);
+    }
+
+    @Override
+    public void onFinishInput() {
+        Log.i(TAG, "onFinishInput()");
+        super.onFinishInput();
     }
 
     @Override
@@ -111,12 +97,6 @@ public class InputMethodServiceWrapper extends InputMethodService {
         if (mCountDownLatch != null && mLatchEvent == EVENT_SHOW) {
             mCountDownLatch.countDown();
         }
-    }
-
-    @Override
-    public void onFinishInput() {
-        Log.i(TAG, "onFinishInput()");
-        super.onFinishInput();
     }
 
     @Override
@@ -146,14 +126,35 @@ public class InputMethodServiceWrapper extends InputMethodService {
         }
     }
 
+    public boolean getCurrentInputViewStarted() {
+        return mInputViewStarted;
+    }
+
+    /**
+     * Sets the latch used to wait for the IME event.
+     *
+     * @param latch      the latch to wait on.
+     * @param latchEvent the event to set the latch on.
+     */
+    public void setCountDownLatchForTesting(@Nullable CountDownLatch latch, @Event int latchEvent) {
+        mCountDownLatch = latch;
+        mLatchEvent = latchEvent;
+    }
+
+    /** Gets the last created instance of this wrapper, if available. */
+    @Nullable
+    public static InputMethodServiceWrapper getInstance() {
+        return sInstance.get();
+    }
+
     /**
      * Gets the string representation of the IME event that is being waited on.
      *
-     * @param event the IME event.
+     * @param eventType the IME event type.
      */
     @NonNull
-    public static String eventToString(@Event int event) {
-        return switch (event) {
+    public static String eventToString(@Event int eventType) {
+        return switch (eventType) {
             case EVENT_SHOW -> "onStartInputView";
             case EVENT_HIDE -> "onFinishInputView";
             case EVENT_CONFIG -> "onConfigurationChanged";
