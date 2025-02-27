6c2197ca8fb4f2c4cf2dd015988ac74a77738b3d
Peter Kalauskas <peskal@google.com>
2025-02-27 10:12:18 -0800

    Move remaining sysui dagger qualifiers to pods
    Test: m, flashall
    Bug: 307607958
    Flag: EXEMPT build change is a no-op
    Change-Id: I45f3efbcae5f47570668f64f296c1d6c9712a238

--- packages/SystemUI/pods/com/android/systemui/dagger/qualifiers/SystemUser.kt
+++ packages/SystemUI/pods/com/android/systemui/dagger/qualifiers/SystemUser.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.android.systemui.dagger.qualifiers
 
 import javax.inject.Qualifier
@@ -21,4 +20,4 @@ import javax.inject.Qualifier
 @Qualifier
 @MustBeDocumented
 @Retention(AnnotationRetention.RUNTIME)
-annotation class NotifInflation
+public annotation class SystemUser
