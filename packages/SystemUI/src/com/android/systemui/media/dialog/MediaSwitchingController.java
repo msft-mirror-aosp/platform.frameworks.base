b267ec1270fb96c9a371c4c58648955513e9e2bd
Derek Jedral <derekjedral@google.com>
2025-03-02 15:45:07 -0800

    Revert "Fix grouped items not appearing if total items changed"
    This reverts commit 5f9a5d159c886a046b1ca8e01e68b4c452921b99.
    
    Reason for revert: Might be the cause of recent regressions
    
    Bug: 399104884
    Change-Id: I17f773156157f048a68c8da5581481e375c2507c

--- packages/SystemUI/src/com/android/systemui/media/dialog/MediaSwitchingController.java
+++ packages/SystemUI/src/com/android/systemui/media/dialog/MediaSwitchingController.java
@@ -648,10 +648,6 @@ public class MediaSwitchingController
             final MediaDevice connectedMediaDevice =
                     needToHandleMutingExpectedDevice ? null
                             : getCurrentConnectedMediaDevice();
-
-            Set<String> selectedDevicesIds = getSelectedMediaDevice().stream()
-                    .map(MediaDevice::getId)
-                    .collect(Collectors.toSet());
             if (oldMediaItems.isEmpty()) {
                 if (connectedMediaDevice == null) {
                     if (DEBUG) {
@@ -660,14 +656,12 @@ public class MediaSwitchingController
                     return categorizeMediaItemsLocked(
                             /* connectedMediaDevice */ null,
                             devices,
-                            selectedDevicesIds,
                             needToHandleMutingExpectedDevice);
                 } else {
                     // selected device exist
                     return categorizeMediaItemsLocked(
                             connectedMediaDevice,
                             devices,
-                            selectedDevicesIds,
                             /* needToHandleMutingExpectedDevice */ false);
                 }
             }
@@ -701,20 +695,9 @@ public class MediaSwitchingController
                 devices.removeAll(targetMediaDevices);
                 targetMediaDevices.addAll(devices);
             }
-            List<MediaItem> finalMediaItems = new ArrayList<>();
-            boolean shouldAddFirstSeenSelectedDevice =
-                    com.android.media.flags.Flags.enableOutputSwitcherDeviceGrouping();
-            for (MediaDevice targetMediaDevice : targetMediaDevices) {
-                if (shouldAddFirstSeenSelectedDevice
-                        && selectedDevicesIds.contains(targetMediaDevice.getId())) {
-                    finalMediaItems.add(MediaItem.createDeviceMediaItem(
-                            targetMediaDevice, /* isFirstDeviceInGroup */ true));
-                    shouldAddFirstSeenSelectedDevice = false;
-                } else {
-                    finalMediaItems.add(MediaItem.createDeviceMediaItem(
-                            targetMediaDevice, /* isFirstDeviceInGroup */ false));
-                }
-            }
+            List<MediaItem> finalMediaItems = targetMediaDevices.stream()
+                    .map(MediaItem::createDeviceMediaItem)
+                    .collect(Collectors.toList());
             dividerItems.forEach(finalMediaItems::add);
             attachConnectNewDeviceItemIfNeeded(finalMediaItems);
             return finalMediaItems;
@@ -741,9 +724,11 @@ public class MediaSwitchingController
     @GuardedBy("mMediaDevicesLock")
     private List<MediaItem> categorizeMediaItemsLocked(MediaDevice connectedMediaDevice,
             List<MediaDevice> devices,
-            Set<String> selectedDevicesIds,
             boolean needToHandleMutingExpectedDevice) {
         List<MediaItem> finalMediaItems = new ArrayList<>();
+        Set<String> selectedDevicesIds = getSelectedMediaDevice().stream()
+                .map(MediaDevice::getId)
+                .collect(Collectors.toSet());
         if (connectedMediaDevice != null) {
             selectedDevicesIds.add(connectedMediaDevice.getId());
         }
