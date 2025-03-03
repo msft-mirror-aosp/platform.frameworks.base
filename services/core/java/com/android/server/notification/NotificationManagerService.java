dc8eef9aade27a28a58d04f7078b262a269f801c
Julia Reynolds <juliacr@google.com>
2025-03-03 06:25:25 -0800

    Revert "Check sound Uri permission when creating a notification channel"
    This reverts commit b4da73a935a8c906ff5df562155824d63ac849ab.
    
    Reason for revert: b/337775777
    
    Change-Id: I7a69c2b1dd3f80dc4778b4175a090a22ded4fc03
    Merged-In: Iaf59e34577bed2eba8da30037a77d15e4f4bd690
    Merged-In: Ic0e6b30ee06ee5d992c6e2b5ae63418b82400b5f

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2346,7 +2346,6 @@ public class NotificationManagerService extends SystemService {
                 mPermissionHelper,
                 mNotificationChannelLogger,
                 mAppOps,
-                mUgmInternal,
                 new SysUiStatsEvent.BuilderFactory(),
                 mShowReviewPermissionsNotification);
         mRankingHelper = new RankingHelper(getContext(),
@@ -5876,7 +5875,13 @@ public class NotificationManagerService extends SystemService {
             final Uri originalSoundUri =
                     (originalChannel != null) ? originalChannel.getSound() : null;
             if (soundUri != null && !Objects.equals(originalSoundUri, soundUri)) {
-                PermissionHelper.grantUriPermission(mUgmInternal, soundUri, sourceUid);
+                Binder.withCleanCallingIdentity(() -> {
+                    mUgmInternal.checkGrantUriPermission(sourceUid, null,
+                            ContentProvider.getUriWithoutUserId(soundUri),
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION,
+                            ContentProvider.getUserIdFromUri(soundUri,
+                            UserHandle.getUserId(sourceUid)));
+                });
             }
         }
 
