7bcb8d1fb18dbc088a387ec201dc4cd227416913
Ibrahim Yilmaz <iyz@google.com>
2025-03-04 00:23:00 -0800

    Revert "Allow public notifications to expand with a swipe"
    This reverts commit ab7355f936bd1779d00cef3f350000448df455ec.
    
    Reason for revert: potentially culprit for b/399742494
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c207179ae2ee179402f89cefc31b12e39a9fa7ff)
    Merged-In: I5a9bccc64da146fe412d964f721e60e5cf557eb9
    Change-Id: I5a9bccc64da146fe412d964f721e60e5cf557eb9

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
@@ -3048,7 +3048,6 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
 
         mUserLocked = userLocked;
         mPrivateLayout.setUserExpanding(userLocked);
-        mPublicLayout.setUserExpanding(userLocked);
         // This is intentionally not guarded with mIsSummaryWithChildren since we might have had
         // children but not anymore.
         if (mChildrenContainer != null) {
