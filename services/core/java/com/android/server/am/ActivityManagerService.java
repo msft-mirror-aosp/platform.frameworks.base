e690fb63636409ae64dcb88df4cc87c7c2850619
Mayank Dandwani <mayankkk@google.com>
2025-03-04 12:02:15 -0800

    Allow core uids to register receiver as "android".
    System components can run in any processes belonging to core uids. If
    one of such components end up registering a receiver, it will get
    registered as "android".
    
    Bug: 387930030
    Test: builds
    Flag: EXEMPT bugfix
    Merged-In: Ied09800f02e03a52efe39f959265da7cb1e1ddc0
    Merged-In: I3fc4c9097602147e44ef77ad80defd33d8f5a2fc
    Change-Id: Ia577940331af5a79804aa6afcd1773e207a58558

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -13154,7 +13154,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                             + " (pid=" + Binder.getCallingPid()
                             + ") when registering receiver " + receiver);
                 }
-                if (callerApp.info.uid != SYSTEM_UID
+                if (!UserHandle.isCore(callerApp.info.uid)
                         && !callerApp.getPkgList().containsKey(callerPackage)) {
                     throw new SecurityException("Given caller package " + callerPackage
                             + " is not running in process " + callerApp);
