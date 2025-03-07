0b0e8ff4a626fb1f0356cee7e0e0f336ff9e8737
Qijing Yao <yaoqq@google.com>
2025-03-07 19:18:02 +0900

    Clear indicator surfaces on drag end for same-display drags
    Using DragPositioningCallbackUtility for same-display drag bounds
    (I6797e11536) prevented the MultiDisplayDragMoveIndicatorController from
    properly clearing indicator surfaces on drag end. This caused potential
    surface leaks when drags started and ended on the same display but
    traversed other displays.
    
    This change ensures that onDragEnd() is always called on the indicator
    controller, guaranteeing that all surfaces are properly disposed of.
    
    Bug: 401464337
    Test: Manual; atest
    Flag: com.android.window.flags.enable_connected_displays_window_drag
    Change-Id: Iebfc4d1b017be695d6d5bbb8ef61670c276eefe9

--- libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/MultiDisplayVeiledResizeTaskPositioner.kt
+++ libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/MultiDisplayVeiledResizeTaskPositioner.kt
@@ -278,12 +278,15 @@ class MultiDisplayVeiledResizeTaskPositioner(
                         currentDisplayLayout,
                     )
                 )
-
-                multiDisplayDragMoveIndicatorController.onDragEnd(
-                    desktopWindowDecoration.mTaskInfo.taskId,
-                    transactionSupplier,
-                )
             }
+
+            // Call the MultiDisplayDragMoveIndicatorController to clear any active indicator
+            // surfaces. This is necessary even if the drag ended on the same display, as surfaces
+            // may have been created for other displays during the drag.
+            multiDisplayDragMoveIndicatorController.onDragEnd(
+                desktopWindowDecoration.mTaskInfo.taskId,
+                transactionSupplier,
+            )
 
             interactionJankMonitor.end(Cuj.CUJ_DESKTOP_MODE_DRAG_WINDOW)
         }
