6b7574f3d90c456b343e74afd6c5126d29b1d392
Jordan Demeulenaere <jdemeulenaere@google.com>
2025-03-10 17:08:07 +0100

    Add NestedDraggable.shouldConsumeNestedPreScroll()
    This CL adds a shouldConsumeNestedPreScroll() callback to
    NestedDraggable so that it can consume scroll events before nested
    scrollable.
    
    This CL also renames shouldConsumedNestedScroll() to
    shouldConsumedNestedPostScroll(), to make it more explicit when it is
    called.
    
    Bug: 397989775
    Test: atest NestedDraggableTest
    Flag: EXEMPT new unused API
    Change-Id: I63b7f3295e22aa0c75f1648d6c2ee409a763bcbd

--- packages/SystemUI/compose/scene/src/com/android/compose/animation/scene/DraggableHandler.kt
+++ packages/SystemUI/compose/scene/src/com/android/compose/animation/scene/DraggableHandler.kt
@@ -68,7 +68,7 @@ internal class DraggableHandler(
         return layoutImpl.swipeDetector.detectSwipe(change)
     }
 
-    override fun shouldConsumeNestedScroll(sign: Float): Boolean {
+    override fun shouldConsumeNestedPostScroll(sign: Float): Boolean {
         return this.enabled()
     }
 
