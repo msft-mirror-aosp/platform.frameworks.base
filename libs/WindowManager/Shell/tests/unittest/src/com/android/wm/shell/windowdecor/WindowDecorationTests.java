7a8a0f0f6b4bbb5bccacb73067bd049a28e194a3
Satish Yalla <satishy@google.com>
2025-03-09 19:59:03 -0700

    Revert "Animate caption showing/hiding"
    This reverts commit b358931338ea9e730b6d3ea48b09c6acdf8cfc04.
    
    Reason for revert: Droidmonitor created revert due to b/401579857.Will be verified through ABTD for standard investigation.
    
    Change-Id: Ia95e18a839a655e31ab7ae5519ccf5e871156799

--- libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/windowdecor/WindowDecorationTests.java
+++ libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/windowdecor/WindowDecorationTests.java
@@ -1186,6 +1186,11 @@ public class WindowDecorationTests extends ShellTestCase {
         }
 
         @Override
+        int getCaptionViewId() {
+            return R.id.caption;
+        }
+
+        @Override
         TestView inflateLayout(Context context, int layoutResId) {
             if (layoutResId == R.layout.caption_layout) {
                 return mMockView;
