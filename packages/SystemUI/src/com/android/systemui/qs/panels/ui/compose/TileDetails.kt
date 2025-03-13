055ad04aa22aca0070eaf449e17dbed275498d41
Andre Le <leandre@google.com>
2025-03-13 21:11:27 +0000

    Reland ""CastDetailsView: Create CastDetailsViewModel and CastDetailsViewContent"
    The original CL was reverted (b/403313655) due to a conflict with another
    CL (ag/31943060). See the diff between PS1 vs PS2 for the reland fix.
    
    Bug: 378514236
    Flag: com.android.systemui.qs_tile_detailed_view
    Test: CastTileTest
    Test: Click on cast tile in the QS -> go into details view instead of
    showing the dialog.
    
    Change-Id: I7da552819ed6528e89e4495833781ae3eb569230

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/TileDetails.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/TileDetails.kt
@@ -43,6 +43,8 @@ import com.android.systemui.bluetooth.qsdialog.BluetoothDetailsViewModel
 import com.android.systemui.plugins.qs.TileDetailsViewModel
 import com.android.systemui.qs.flags.QsDetailedView
 import com.android.systemui.qs.panels.ui.viewmodel.DetailsViewModel
+import com.android.systemui.qs.tiles.dialog.CastDetailsContent
+import com.android.systemui.qs.tiles.dialog.CastDetailsViewModel
 import com.android.systemui.qs.tiles.dialog.InternetDetailsContent
 import com.android.systemui.qs.tiles.dialog.InternetDetailsViewModel
 import com.android.systemui.qs.tiles.dialog.ModesDetailsContent
@@ -131,6 +133,7 @@ private fun MapTileDetailsContent(tileDetailsViewModel: TileDetailsViewModel) {
         is BluetoothDetailsViewModel ->
             BluetoothDetailsContent(tileDetailsViewModel.detailsContentViewModel)
         is ModesDetailsViewModel -> ModesDetailsContent(tileDetailsViewModel)
+        is CastDetailsViewModel -> CastDetailsContent()
     }
 }
 
