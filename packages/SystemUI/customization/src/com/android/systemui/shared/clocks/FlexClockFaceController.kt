2626bcf4e30c0f347b5969a97328275459e05bcd
Hawkwood <jglazier@google.com>
2025-03-14 17:27:05 +0000

    Animate changes to font axes for picker preview changes
    Bug: 395647577
    Test: Manually validated animation functions
    Flag: com.android.systemui.shared.clock_reactive_variants
    Change-Id: Ie1caec653a3483adf6a7d998a9adff22d8904407

--- packages/SystemUI/customization/src/com/android/systemui/shared/clocks/FlexClockFaceController.kt
+++ packages/SystemUI/customization/src/com/android/systemui/shared/clocks/FlexClockFaceController.kt
@@ -31,10 +31,12 @@ import com.android.systemui.plugins.clocks.ClockFaceConfig
 import com.android.systemui.plugins.clocks.ClockFaceController
 import com.android.systemui.plugins.clocks.ClockFaceEvents
 import com.android.systemui.plugins.clocks.ClockFaceLayout
+import com.android.systemui.plugins.clocks.ClockFontAxis.Companion.merge
 import com.android.systemui.plugins.clocks.DefaultClockFaceLayout
 import com.android.systemui.plugins.clocks.ThemeConfig
 import com.android.systemui.plugins.clocks.WeatherData
 import com.android.systemui.plugins.clocks.ZenData
+import com.android.systemui.shared.clocks.FlexClockController.Companion.getDefaultAxes
 import com.android.systemui.shared.clocks.FontUtils.get
 import com.android.systemui.shared.clocks.FontUtils.set
 import com.android.systemui.shared.clocks.ViewUtils.computeLayoutDiff
@@ -131,15 +133,6 @@ class FlexClockFaceController(clockCtx: ClockContext, private val isLargeClock: 
             layerController.faceEvents.onThemeChanged(theme)
         }
 
-        override fun onFontAxesChanged(settings: ClockAxisStyle) {
-            var axes = ClockAxisStyle(settings)
-            if (!isLargeClock && axes[GSFAxes.WIDTH] > SMALL_CLOCK_MAX_WDTH) {
-                axes[GSFAxes.WIDTH] = SMALL_CLOCK_MAX_WDTH
-            }
-
-            layerController.events.onFontAxesChanged(axes)
-        }
-
         /**
          * targetRegion passed to all customized clock applies counter translationY of Keyguard and
          * keyguard_large_clock_top_margin from default clock
@@ -231,6 +224,15 @@ class FlexClockFaceController(clockCtx: ClockContext, private val isLargeClock: 
 
             override fun onFidgetTap(x: Float, y: Float) {
                 layerController.animations.onFidgetTap(x, y)
+            }
+
+            override fun onFontAxesChanged(style: ClockAxisStyle) {
+                var axes = ClockAxisStyle(getDefaultAxes(clockCtx.settings).merge(style))
+                if (!isLargeClock && axes[GSFAxes.WIDTH] > SMALL_CLOCK_MAX_WDTH) {
+                    axes[GSFAxes.WIDTH] = SMALL_CLOCK_MAX_WDTH
+                }
+
+                layerController.animations.onFontAxesChanged(axes)
             }
         }
 
