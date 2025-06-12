30dc386d4a57fce3ee2e7be3030cf80db81ae204
Aseem Kumar <aseemk@google.com>
2025-06-11 18:41:20 -0700

    Don't allow SdkSandbox to bypass systemUid check.
    Test: atest
    
    Flag: EXEMPT security fix
    
    Bug: 397438392
    (cherry picked from commit d400709160c8374d83a15dc7623b11434c08c4c6)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:2950685774c5987145aae55adde18bab182ab7a7)
    Merged-In: I1003ac2a795c869aaeb292692b2681c7a5c8d12e
    Change-Id: I1003ac2a795c869aaeb292692b2681c7a5c8d12e

--- services/core/java/com/android/server/accounts/AccountManagerService.java
+++ services/core/java/com/android/server/accounts/AccountManagerService.java
@@ -5985,6 +5985,9 @@ public class AccountManagerService
     }
 
     private boolean isSystemUid(int callingUid) {
+        if (Process.isSdkSandboxUid(callingUid)) {
+            return false;
+        }
         String[] packages = null;
         final long ident = Binder.clearCallingIdentity();
         try {
