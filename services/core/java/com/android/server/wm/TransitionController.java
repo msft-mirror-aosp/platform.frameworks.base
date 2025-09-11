caffcf6be82b0aea04fe746907920500f1f2c81d
Evan Rosky <erosky@google.com>
2025-09-11 15:23:52 -0700

    Map animation delegate to the transition instead of plumbing it
    Previously we were plumbing the animation delegate into WM and
    then back-out through Request and then back-in again when we
    actually wanted to boost the thread priority.
    
    Instead of doing that, record the delegate in WM and then shell
    only has to notify when to boost. This prevents mismatches, is
    more robust to situations where we don't have a handleRequest,
    and removes the need to send an IApplicationThread token out
    of WMCore.
    
    Bug: 438742644
    Test: atest ActivityManagerTest#testActivityManager_stripsAppThreadFromRemoteTransition
    Flag: EXEMPT CVE_FIX
    DISABLE_TOPIC_PROTECTOR
    (cherry picked from commit a35ab54ec741fb24370ef543d79d44d07823a292)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:e3caedb88d32bd7dd93e822b3853a532ae5a3bc1
    Merged-In: Ie7d28d52fe37177ad10f8969b9d8cc6594cbc41c
    Change-Id: Ie7d28d52fe37177ad10f8969b9d8cc6594cbc41c

--- services/core/java/com/android/server/wm/TransitionController.java
+++ services/core/java/com/android/server/wm/TransitionController.java
@@ -699,8 +699,18 @@ class TransitionController {
                 info = new ActivityManager.RunningTaskInfo();
                 startTask.fillTaskInfo(info);
             }
+
+            final RemoteTransition remoteInfo;
+            if (remoteTransition != null) {
+                transition.mRemoteDelegate = remoteTransition.getAppThread();
+                remoteInfo = new RemoteTransition(remoteTransition.getRemoteTransition(),
+                        remoteTransition.getDebugName());
+            } else {
+                remoteInfo = null;
+            }
+
             final TransitionRequestInfo request = new TransitionRequestInfo(
-                    transition.mType, info, remoteTransition, displayChange);
+                    transition.mType, info, remoteInfo, displayChange);
             transition.mLogger.mRequestTimeNs = SystemClock.elapsedRealtimeNanos();
             transition.mLogger.mRequest = request;
             mTransitionPlayer.requestStartTransition(transition.getToken(), request);
