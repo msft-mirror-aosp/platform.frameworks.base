0b59dbb2ad3031f867c1f2eec1f1cd6946f5dd86
Evan Rosky <erosky@google.com>
2025-09-11 15:23:52 -0700

    Map animation delegate to the transition instead of plumbing it
    Previously we were plumbing the animation delegate into WM and
    then back-out through Request and then back-in again when we
    actually wanted to boost the thread priority.
    
    Instead of doing that, record the delegate in WM and then shell
    only has to notify when to boost. This prevents mismatches, is
    more robust to situations where we don't have a handleRequest,
    and removes the need to send an IApplicationThread token out
    of WMCore.
    
    Bug: 438742644
    Test: atest ActivityManagerTest#testActivityManager_stripsAppThreadFromRemoteTransition
    Flag: EXEMPT CVE_FIX
    DISABLE_TOPIC_PROTECTOR
    (cherry picked from commit a35ab54ec741fb24370ef543d79d44d07823a292)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:a29de493b054518b02e4da0ce7ad34748c4dea76
    Merged-In: Ie7d28d52fe37177ad10f8969b9d8cc6594cbc41c
    Change-Id: Ie7d28d52fe37177ad10f8969b9d8cc6594cbc41c

--- libs/WindowManager/Shell/src/com/android/wm/shell/splitscreen/StageCoordinator.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/splitscreen/StageCoordinator.java
@@ -987,7 +987,6 @@ public class StageCoordinator implements SplitLayout.SplitLayoutHandler,
                                 finishedCallback.onAnimationFinished();
                             }
                         };
-                Transitions.setRunningRemoteTransitionDelegate(adapter.getCallingApplication());
                 try {
                     adapter.getRunner().onAnimationStart(transit, apps, wallpapers,
                             ArrayUtils.appendElement(RemoteAnimationTarget.class, nonApps,
@@ -1052,7 +1051,6 @@ public class StageCoordinator implements SplitLayout.SplitLayoutHandler,
                                     finishedCallback.onAnimationFinished();
                                 }
                             };
-                    Transitions.setRunningRemoteTransitionDelegate(adapter.getCallingApplication());
                     try {
                         adapter.getRunner().onAnimationStart(
                                 transit, apps, wallpapers, nonApps, wrapCallback);
