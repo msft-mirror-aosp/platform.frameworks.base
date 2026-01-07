88b8f2c4a9b7b2e008335a654b1b5a1fcf39f669
Riddle Hsu <riddlehsu@google.com>
2026-01-07 16:48:51 +0800

    Apply visibility policy to window state surface
    The client side can access the ViewRootImpl surface and changes
    its visibility, so the server side visibility policy should control
    the visibility of the surface which is not accessible from the
    client side, i.e. the surface of window state instead of the
    client surface.
    
    Bug: 452010556
    Flag: EXEMPT BUGFIX
    Test: atest WindowStateTests#testIsOnScreen_hiddenByPolicy
    Test: Show an overlay window and loop to show the VRI surface.
          Launch Settings. The overlay window should not be visible.
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:e58fd9cd11e400235ac480b4fba6a20f8161ff9b
    Merged-In: I326fcaf71f901a11cf4aee3d734494090a1d0fb3
    Change-Id: I326fcaf71f901a11cf4aee3d734494090a1d0fb3

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -2802,6 +2802,9 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
             }
             if (!isVisibleByPolicy()) {
                 mWinAnimator.hide(getGlobalTransaction(), "checkPolicyVisibilityChange");
+                if (mSurfaceControl != null) {
+                    getPendingTransaction().hide(mSurfaceControl);
+                }
                 if (isFocused()) {
                     ProtoLog.i(WM_DEBUG_FOCUS_LIGHT,
                             "setAnimationLocked: setting mFocusMayChange true");
@@ -3078,6 +3081,9 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
         }
         setPolicyVisibilityFlag(LEGACY_POLICY_VISIBILITY);
         mLegacyPolicyVisibilityAfterAnim = true;
+        if (mSurfaceControl != null) {
+            getPendingTransaction().show(mSurfaceControl);
+        }
         if (doAnimation) {
             mWinAnimator.applyAnimationLocked(TRANSIT_ENTER, true);
         }
@@ -3124,6 +3130,9 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
                 ProtoLog.i(WM_DEBUG_FOCUS_LIGHT,
                         "WindowState.hideLw: setting mFocusMayChange true");
                 mWmService.mFocusMayChange = true;
+            }
+            if (mSurfaceControl != null) {
+                getPendingTransaction().hide(mSurfaceControl);
             }
         }
         if (requestAnim) {
